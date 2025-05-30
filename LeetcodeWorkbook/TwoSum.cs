927b3b48b03439bc2322c1bfa50d66ea77722539
FunctionJoey <functionjoey@gmail.com>
2025-05-30 14:39:57 -0600

    TwoSum first attempt plus suggested solution

--- LeetcodeWorkbook/TwoSum.cs
+++ LeetcodeWorkbook/TwoSum.cs
@@ -4,6 +4,36 @@ using System;
 
 class TwoSum
 {
+    // Runs the tests
+    public static void run()
+    {
+        //Test case 1
+        int[] arr1 = { 2, 7, 11, 15 };
+        int target1 = 9;
+        printIntArray(twoSum(arr1, target1));
+
+        //Test case 2
+        int[] arr2 = { 3, 2, 4 };
+        int target2 = 6;
+        printIntArray(twoSum(arr2, target2));
+
+        //Test case 3
+        int[] arr3 = { 3, 3 };
+        int target3 = 6;
+        printIntArray(twoSum(arr3, target3));
+
+        //Test case 4
+        int[] arr4 = { 2, 4, 11, 3 };
+        int target4 = 6;
+        printIntArray(twoSum(arr4, target4));
+
+        //Test case 5
+        int[] arr5 = { 3, 2, 4 };
+        int target5 = 6;
+        printIntArray(twoSum(arr5, target5));
+    }
+
+    // Prints the array
     public static void printIntArray(int[] arr)
     {
         Console.Write("[");
@@ -21,31 +51,67 @@ class TwoSum
         Console.Write("]");
     }
 
-    public static void run()
-    {
-        //Test case 1
-        int[] arr1 = { 2, 7, 11, 15 };
-        int target1 = 9;
-        printIntArray(twoSum(arr1, target1));
+    // --------------------TwoSum: New attempts, past attempts, and suggested solutions
+    // New attempts go here 
 
-        //Test case 2
-        int[] arr2 = { 3, 2, 4 };
-        int target2 = 6;
-        printIntArray(twoSum(arr2, target2));
-
-        //Test case 3
-        int[] arr3 = { 3, 3 };
-        int target3 = 6;
-        printIntArray(twoSum(arr3, target3));
-    }
-
+    // Accepted -> beats 96.19% (Memory) and 10.64% (Runtime)
     public static int[] twoSum(int[] nums, int target)
     {
-        
-
         int[] arr = { 0, 0 };
+        bool targetReached = false;
+
+        Console.WriteLine();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                Console.WriteLine(nums[i] + " + " + nums[j] + " = " + (nums[i] + nums[j]));
+                if ((nums[i] + nums[j]).Equals(target) && i != j)
+                {
+                    arr[0] = i;
+                    arr[1] = j;
+                    targetReached = true;
+                    break;
+                }
+            }
+            if (targetReached == true)
+            {
+                break;
+            }
+        }
 
         return arr;
     }
 
+    // Suggested Solution (Otabek via Leetcode)
+    /*
+     *
+        Approach
+
+        1. Create a hash table (num_map) to store numbers along with their indices.
+        
+        2. Iterate through the array:
+            Compute the complement of the current number (target - num).
+            Check if the complement is already in the hash table.
+            If it is, return the indices of the complement and the current number.
+            Otherwise, store the current number with its index in the hash table.
+        
+        3. Since there is exactly one solution, we are guaranteed to find a valid pair.
+     *
+     */
+        public int[] TwoSumSuggested(int[] nums, int target)
+    {
+        Dictionary<int, int> numMap = new Dictionary<int, int>(); // A mapping to store numbers and their indices
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i]; // Find the required number to reach the target
+            if (numMap.ContainsKey(complement))
+            {
+                return new int[] { numMap[complement], i }; // Return indices of the complement and current number
+            }
+            numMap[nums[i]] = i; // Store the number with its index
+        }
+        return new int[] { }; // This line is never reached due to the problem guarantee
+    }
 }
--- LeetcodeWorkbook/obj/Debug/net9.0/LeetcodeWorkbook.AssemblyInfo.cs
+++ LeetcodeWorkbook/obj/Debug/net9.0/LeetcodeWorkbook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetcodeWorkbook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8cfbb67b561928a1c8c0d6eb016a4b4d2b6c80f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7efd327ce3f6a0340e7be012d333f28c7e69be9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetcodeWorkbook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetcodeWorkbook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
